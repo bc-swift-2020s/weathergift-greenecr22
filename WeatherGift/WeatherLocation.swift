5793e0a205a28110f22a7788ddff400f1c0d002c
Christopher Greene <greenecr@bc.edu>
2020-03-09 16:19:28 -0400

    7.2 complete

---
+++ WeatherGift/WeatherLocation.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherLocation.swift
+//  WeatherGift
+//
+//  Created by Christopher Greene on 3/9/20.
+//  Copyright © 2020 Christopher Greene. All rights reserved.
+//
+
+import Foundation
+
+
+struct WeatherLocation {
+    var name: String
+    var latitude: Double
+    var longitude: Double
+    
+}
