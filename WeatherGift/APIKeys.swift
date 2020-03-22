c702f9dc190cbaeb14a007c18b30c05c7630a848
Christopher Greene <greenecr@bc.edu>
2020-03-22 12:05:15 -0400

    7.4 with gitignore

---
+++ WeatherGift/APIKeys.swift
@@ -0,0 +1,13 @@
+//
+//  APIKeys.swift
+//  WeatherGift
+//
+//  Created by Christopher Greene on 3/22/20.
+//  Copyright © 2020 Christopher Greene. All rights reserved.
+//
+
+import Foundation
+
+struct APIKeys {
+    static let googlePlacesKey = "AIzaSyCuFg55P5FRAYf0cysTFDrqYHquPk7nmlI"
+}
